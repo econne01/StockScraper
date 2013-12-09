7df562600ae0880758650ac9c347a98206ce5679
eric <eric.connelly@essencedigital.com>
2013-12-08 20:40:24 -0500

    Wrote a setup.py file to allow installing from buildout

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'StockScraper',
+    version = '0.1',
+    author = 'Gurchet Rai',
+    description = 'Retreive stock quote information from Yahoo Finance API',
+    url = 'https://github.com/gurch101/StockScraper',
+    packages = find_packages(),
+)
