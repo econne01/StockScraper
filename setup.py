0590bf647fa9d4adbf04b434601cb74141000213
econne01 <=eric.connelly08@gmail.com>
2013-12-14 17:52:03 -0500

    Change setup to use original source url

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version = '0.1',
     author = 'Gurchet Rai',
     description = 'Retreive stock quote information from Yahoo Finance API',
-    url = 'https://github.com/econne01/StockScraper',
+    url = 'https://github.com/gurch101/StockScraper',
     packages = find_packages(),
 )
